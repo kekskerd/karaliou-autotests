48de366c4f92bb3e34bffb0f1025c0e727943299
Korolev Dmitriy <kekskerd@gmail.com>
2021-06-14 10:56:58 +0300

    AutoLogIn task done

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,36 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        WebDriverManager.chromedriver().setup();
+        ChromeDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://habr.com/");
+        WebElement logIn = driver.findElement(By.id("login"));
+        logIn.click();
+        WebElement emailField = driver.findElement(By.id("email_field"));
+        emailField.sendKeys("testuser1312test@yandex.ru");
+        WebElement passwordField = driver.findElement(By.id("password_field"));
+        passwordField.sendKeys("testuser1312test1312");
+        WebElement enterButton = driver.findElement(By.cssSelector("button.button_wide.button_primary"));
+        enterButton.click();
+        driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+        try {
+            WebElement profileButton = driver.findElement(By.cssSelector("button.btn_navbar_user-dropdown"));
+            profileButton.click();
+        } catch (Exception e) {
+            driver.quit();
+            throw new Exception("Authentication error");
+        }
+        System.out.println("Successful test");
+        driver.quit();
     }
 }
