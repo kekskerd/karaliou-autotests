29241786b4b7fcb34ff120b99f45bc5f87a04fba
Korolev Dmitriy <kekskerd@gmail.com>
2021-06-23 17:33:50 +0300

    refactoring BaseTest

--- src/test/java/org/example/BaseTest.java
+++ src/test/java/org/example/BaseTest.java
@@ -14,36 +14,25 @@ import org.testng.annotations.*;
 import pages.HomePage;
 import pages.LogInPage;
 import utils.PropertyHelper;
+
 import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @Listeners(DefaultListener.class)
 public class BaseTest {
-     WebDriver driver;
+    WebDriver driver;
     private final String startUrl = PropertyHelper.getConf().startUrl();
-//    private final int pageLoadTimeout = PropertyHelper.getConf().pageLoadTimeout();
-//    private final int elementTimeout = PropertyHelper.getConf().elementTimeout();
 
     @BeforeTest
     public void setUp() {
         driver = DriverProvider.getDriver();
         log.info("Open url: " + startUrl);
         driver.get(startUrl);
-
-//        WebDriverManager.chromedriver().setup();
-//        driver = new ChromeDriver();
-//        driver.manage().window().maximize();
-//        driver.manage().timeouts().implicitlyWait(elementTimeout, TimeUnit.SECONDS);
-//        driver.manage().timeouts().pageLoadTimeout(pageLoadTimeout, TimeUnit.SECONDS);
-//        driver.get(startUrl);
     }
+
     @AfterClass
     public void afterTest() {
         log.info("Tear down driver");
         DriverProvider.tearDown();
     }
-//    @AfterTest
-//    public void quitTest() {
-//        driver.quit();
-//    }
 }
